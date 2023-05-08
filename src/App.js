49c015a7621421237b8da63411f7aaa3c805af50
Soaib Akhtar <98397413+soaibakhtar@users.noreply.github.com>
2023-05-08 22:14:49 +0530

    Complete  Todo App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import { useState } from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import "./styles.css"
-
+import {AiOutlineSend,AiFillDelete} from "react-icons/ai"
 function App() {
 
-  const [todos,setTodos] = useState([]);
+  const [todos,setTodos] = useState(JSON.parse(localStorage.getItem("todos")));
   const [newText,setNewText] = useState("")
+
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    setTodos([...todos,{text : newText,completed : false}])
+    setNewText("")
+  }
+
+  const handelDelete = (index) => {
+    const newTodos = [...todos]
+    newTodos.splice(index,1)
+    setTodos(newTodos)
+  }
+
+  useEffect(() => {
+    localStorage.setItem("todos",JSON.stringify(todos))
+  },[todos])
 
   return (
     <div className="App">
       <div className='container'>
         <h1>Schedule-Me</h1>
-        <form>
+        <form onSubmit={handelSubmit}>
           <input 
             onChange={(event) => setNewText(event.target.value)}
             className='input-box'
@@ -20,7 +35,34 @@ function App() {
             value={newText}
             required
           />
+          <button type='submit'><AiOutlineSend/></button>
         </form>
+
+        <div className='todo-item'>
+          {todos.map((todo,index)=>{
+            return (
+              <div key={index} className='todo-table'>
+                  <span style={{textDecoration : todo.completed ? "line-through" : "none"}}>
+                    {todo.text}
+                  </span>
+
+                  <div className='check-box'>
+                    <input
+                      type='checkbox'
+                      checked={todo.completed}
+                      onChange={() => {
+                        const newTodo = [...todos]
+                        newTodo[index].completed = !newTodo[index].completed
+                        setTodos(newTodo)
+                      }}
+                    />
+
+                    {todo.completed ? (<button onClick={() => handelDelete(index)} className='delete-btn'><AiFillDelete/></button>) : null}
+                  </div>
+              </div>
+            )
+          })}
+        </div>
       </div>
     </div>
   );
